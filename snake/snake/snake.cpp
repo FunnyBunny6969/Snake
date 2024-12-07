e672ea715c8c5c64c873355314778c8a65685e8a
NOBODY1011986 <nobody1011986@gmail.com>
2024-12-07 22:54:21 +0300

    started a project

---
+++ snake/snake/snake.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+void gotoStart() {
+	COORD coord = { static_cast<SHORT>(0), static_cast<SHORT>(0) };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void setScreen() {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_FONT_INFOEX cfi;
+    SMALL_RECT windowSize = { 0, 0, 80, 30 }; // (левая, верхняя, правая, нижняя)
+    SetConsoleWindowInfo(hConsole, TRUE, &windowSize);
+}
+
+void setFont() {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_FONT_INFOEX cfi;
+    cfi.cbSize = sizeof(cfi);
+    cfi.dwFontSize.Y = 24; // Высота шрифта
+    cfi.dwFontSize.X = 0; // Ширина шрифта
+    cfi.FontFamily = FF_DONTCARE; // Тип шрифта
+    cfi.FontWeight = FW_BOLD; // Нормальная жирность
+    wcscpy_s(cfi.FaceName, L"Consolas"); // Имя шрифта
+    // Устанавливаем шрифт
+    SetCurrentConsoleFontEx(hConsole, FALSE, &cfi);
+}
+
+
+void main() {
+    setScreen();
+    setFont();
+
+    for (int i = 0;i < 80;i++)
+        cout << "#";
+
+
+    cin.get();
+    system("cls");
+    system("pause");
+}
